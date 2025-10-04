2bcbc892582a0b59f178e9c56c76ec3280d7ba96
mchiwundura <mchiwundura@protonmail.com>
2025-10-04 18:29:09 +0200

    Added a GlassSurface object and wrapped the nav with it, also fixed border radius on cta button

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,27 +1,34 @@
 import "./Navbar.css";
 import Ctabutton from "./components/Ctabutton";
+import GlassSurface from './components/GlassSurface'
 
 function Navbar() {
 	return (
-		<nav className="navbar">
-			<div className="navbar-logo">
-				<a href="/">Fundeka</a>
-			</div>
-			<ul className="navbar-links">
-				<li>
-					<a href="#features">Features</a>
-				</li>
-				<li>
-					<a href="#pricing">Pricing</a>
-				</li>
-				<li>
-					<a href="#contact">Contact</a>
-				</li>
-				<li>
-					<Ctabutton title={"Signup"} link={"facebook.com"} />
-				</li>
-			</ul>
-		</nav>
+		<GlassSurface
+			borderRadius={24}
+			width={"90%"}
+			style={{ position: "fixed", top: "20px", zIndex: 1000, left: "50%", transform: "translateX(-50%)" }}
+		>
+			<nav className="navbar">
+				<div className="navbar-logo">
+					<a href="/">FUNDEKA</a>
+				</div>
+				<ul className="navbar-links">
+					<li className="link">
+						<a href="#features">Features</a>
+					</li>
+					<li className="link">
+						<a href="#pricing">Pricing</a>
+					</li>
+					<li className="link">
+						<a href="#contact">Contact</a>
+					</li>
+					<li>
+						<Ctabutton title={"Signup"} link={"facebook.com"} />
+					</li>
+				</ul>
+			</nav>
+		</GlassSurface>
 	);
 }
 
